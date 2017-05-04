c53e2c9f0b0df5bcfc7f2cfc51f036aba9d1f283
Reid Chatham <reid.chatham@gmail.com>
2017-05-03 21:22:04 -0700

    add viewController(configuredWith url: URL) to URLCoordinatorType

--- CoordinatorType-iOS/URLCoordinatorType.swift
+++ CoordinatorType-iOS/URLCoordinatorType.swift
@@ -9,5 +9,12 @@
 import UIKit
 
 public protocol URLCoordinatorType: CoordinatorType {
+    func viewController(configuredWith url: URL) -> UIViewController
     func start(onViewController viewController: UIViewController, withURL url: URL, animated: Bool)
+}
+
+extension URLCoordinatorType {
+    public func start(onViewController viewController: UIViewController, withURL url: URL, animated: Bool) {
+        viewController.present(self.viewController(configuredWith: url), animated: animated, completion: nil)
+    }
 }
